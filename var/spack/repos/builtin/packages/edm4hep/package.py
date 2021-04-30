6914c9d5ef34fd039da09df22cc911dafc3f5cf2
Valentin Volkl <valentin.volkl@cern.ch>
2021-04-30 09:45:36 +0200

    update ROOT_INCLUDE_PATH for new root version

--- var/spack/repos/builtin/packages/edm4hep/package.py
+++ var/spack/repos/builtin/packages/edm4hep/package.py
@@ -42,6 +42,9 @@ class Edm4hep(CMakePackage):
         args.append(self.define("BUILD_TESTING", self.run_tests))
         return args
 
+    def setup_run_environment(self, env):
+        env.prepend_path('ROOT_INCLUDE_PATH', self.prefix.include)
+
     def url_for_version(self, version):
         """Translate version numbers to ilcsoft conventions.
         in spack, the convention is: 0.1 (or 0.1.0) 0.1.1, 0.2, 0.2.1 ...
