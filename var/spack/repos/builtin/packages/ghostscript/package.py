708484c345973e2a482b4da1946f243947396c25
Valentin Volkl <valentin.volkl@cern.ch>
2021-07-07 16:02:32 +0200

    ghostscript: add patch and disable tests

--- var/spack/repos/builtin/packages/ghostscript/package.py
+++ var/spack/repos/builtin/packages/ghostscript/package.py
@@ -35,6 +35,7 @@ class Ghostscript(AutotoolsPackage):
     depends_on('gtkplus')
 
     patch('nogoto.patch', when='%fj@:4.1.0')
+    patch("freetype-2.10.3.patch")
 
     def url_for_version(self, version):
         baseurl = "https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs{0}/ghostscript-{1}.tar.gz"
