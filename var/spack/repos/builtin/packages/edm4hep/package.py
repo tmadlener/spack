30b3d6308edebd4a86e8f5e83719ea7e3ccc5b03
Valentin Volkl <valentin.volkl@cern.ch>
2022-09-24 20:53:57 +0200

    Workaround for faulty test dependency concretization

--- var/spack/repos/builtin/packages/edm4hep/package.py
+++ var/spack/repos/builtin/packages/edm4hep/package.py
@@ -82,8 +82,8 @@ class Edm4hep(CMakePackage):
     depends_on("py-pyyaml", type="build")
 
     depends_on("hepmc@:2", type="test", when="@:0.4.0")
-    depends_on("hepmc3", type="test", when="@0.4.1:")
-    depends_on("heppdt", type="test")
+    depends_on("hepmc3", when="@0.4.1:")
+    depends_on("heppdt")
     depends_on("catch2@3.0.1:", type="test")
 
     def cmake_args(self):
